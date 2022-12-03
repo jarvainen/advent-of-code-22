24283f378cd02fdeb05883fb31ab8edffb58ea30
jarvainen <jarvainen@gmail.com>
2022-12-03 07:29:44 +0200

    Solution for day 3.

--- calendar/3_day_three/solution.go
+++ calendar/3_day_three/solution.go
@@ -1,13 +1,73 @@
 package __day_three
 
-import "fmt"
+import (
+	"embed"
+	"fmt"
+	"github.com/jarvainen/advent-of-code-22/calendar/util"
+	"strings"
+)
+
+//go:embed input.txt
+var content embed.FS
+
+func getItemTypePriority(itemType string) int {
+	typeListUpperCase := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	typeListLowerCase := strings.ToLower(typeListUpperCase)
+	typeList := typeListLowerCase + typeListUpperCase
+	return strings.Index(typeList, itemType) + 1
+}
+
+func findStringIntersection(first string, second string) (result string) {
+	for _, firstCharacter := range first {
+		for _, secondCharacter := range second {
+			if firstCharacter == secondCharacter {
+				return fmt.Sprintf("%c", firstCharacter)
+			}
+		}
+	}
+	return ""
+}
+
+func findThreeStringIntersection(first string, second string, third string) (result string) {
+	for _, firstCharacter := range first {
+		for _, secondCharacter := range second {
+			for _, thirdCharacter := range third {
+				if firstCharacter == secondCharacter && firstCharacter == thirdCharacter {
+					return fmt.Sprintf("%c", firstCharacter)
+				}
+			}
+		}
+	}
+	return ""
+}
 
 func silver() {
-	fmt.Println("Not implemented, day 3.")
+	totalPriority := 0
+	util.ReadFileByLine(content, "input.txt", func(line string) {
+		halfIndex := len(line) / 2
+		firstCompartment := line[:halfIndex]
+		secondCompartment := line[halfIndex:]
+		compartmentType := findStringIntersection(firstCompartment, secondCompartment)
+		totalPriority += getItemTypePriority(compartmentType)
+	})
+	fmt.Printf("Solution is %d\n", totalPriority)
 }
 
 func gold() {
-	fmt.Println("Not implemented, day 3.")
+	totalPriority := 0
+	var badgeGroup []string
+	util.ReadFileByLine(content, "input.txt", func(line string) {
+		if len(badgeGroup) < 3 {
+			badgeGroup = append(badgeGroup, line)
+		}
+		if len(badgeGroup) == 3 {
+			badgeType := findThreeStringIntersection(badgeGroup[0], badgeGroup[1], badgeGroup[2])
+			totalPriority += getItemTypePriority(badgeType)
+			badgeGroup = make([]string, 0)
+		}
+
+	})
+	fmt.Printf("Solution is %d\n", totalPriority)
 }
 
 func Solution() {
