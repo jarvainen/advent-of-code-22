b5a42b7488f40902c7b3317e808daafa1d955459
jarvainen <jarvainen@gmail.com>
2022-12-06 07:13:33 +0200

    Solution for day 6.

--- calendar/6_day_six/solution.go
+++ calendar/6_day_six/solution.go
@@ -1,13 +1,55 @@
-package __day_one
+package __day_six
 
-import "fmt"
+import (
+	"embed"
+	"fmt"
+	"github.com/jarvainen/advent-of-code-22/calendar/util"
+)
+
+//go:embed input.txt
+var content embed.FS
+
+func hasAllUniqueCharacters(str string) bool {
+	charMap := map[rune]bool{}
+	for _, char := range str {
+		_, ok := charMap[char]
+		if ok {
+			return false
+		}
+		charMap[char] = true
+	}
+	return true
+}
+
+func findMarker(stream string, consecutiveLength int) int {
+	recentCharacters := ""
+	for index, char := range stream {
+		if len(recentCharacters) == consecutiveLength {
+			if hasAllUniqueCharacters(recentCharacters) {
+				return index
+			} else {
+				recentCharacters = recentCharacters[1:]
+			}
+		}
+		recentCharacters += fmt.Sprintf("%c", char)
+	}
+	return -1
+}
 
 func silver() {
-	fmt.Println("Not implemented")
+	solution := 0
+	util.ReadFileByLine(content, "input.txt", func(line string) {
+		solution = findMarker(line, 4)
+	})
+	fmt.Printf("Solution is %d\n", solution)
 }
 
 func gold() {
-	fmt.Println("Not implemented")
+	solution := 0
+	util.ReadFileByLine(content, "input.txt", func(line string) {
+		solution = findMarker(line, 14)
+	})
+	fmt.Printf("Solution is %d\n", solution)
 }
 
 func Solution() {
--- calendar/solution_list.go
+++ calendar/solution_list.go
@@ -6,6 +6,7 @@ import (
 	__day_three "github.com/jarvainen/advent-of-code-22/calendar/3_day_three"
 	__day_four "github.com/jarvainen/advent-of-code-22/calendar/4_day_four"
 	__day_five "github.com/jarvainen/advent-of-code-22/calendar/5_day_five"
+	__day_six "github.com/jarvainen/advent-of-code-22/calendar/6_day_six"
 )
 
 type Solution func()
@@ -16,4 +17,5 @@ var Solutions = [24]Solution{
 	__day_three.Solution,
 	__day_four.Solution,
 	__day_five.Solution,
+	__day_six.Solution,
 }
